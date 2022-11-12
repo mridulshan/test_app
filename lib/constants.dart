82e973075658ffdf0c73e680d0ae014fd7ff0370
mridul <mridul127cb@gmail.com>
2022-11-12 23:37:52 +0530

    first

---
+++ lib/constants.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+const kinactiveColor = Color(0xFF035aa7);
+const kactiveColor = Color(0xFF5e9cc8);
+const TextStyle ktitleStyle = TextStyle(
+  color: Colors.white,
+  fontSize: 25,
+  fontWeight: FontWeight.w400,
+  decoration: TextDecoration.none,
+);
+const TextStyle ksmallStyle = TextStyle(
+  color: Colors.white,
+  fontSize: 15,
+  fontWeight: FontWeight.w400,
+  decoration: TextDecoration.none,
+);
+
+const TextStyle kcardStyle = TextStyle(
+  color: Colors.black,
+  fontSize: 25,
+  fontWeight: FontWeight.w400,
+  decoration: TextDecoration.none,
+);
+const TextStyle kcardLabelStyle = TextStyle(
+  color: Colors.black,
+  fontSize: 20,
+  fontWeight: FontWeight.w400,
+  decoration: TextDecoration.none,
+);
--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,12 @@
 import 'dart:ui';
+import 'package:flutter/cupertino.dart';
 
+import 'constants.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/rendering.dart';
 
 void main() {
   runApp(MyApp());
 }
-
-const kinactiveColor=Color(0xFF035aa7);
-const kactiveColor = Color(0xFF5e9cc8);
-const TextStyle ktitleStyle = TextStyle(
-    color: Colors.white,
-    fontSize: 25,
-    fontWeight: FontWeight.w400,
-    decoration: TextDecoration.none);
-const TextStyle ksmallStyle = TextStyle(
-    color: Colors.white,
-    fontSize: 15,
-    fontWeight: FontWeight.w400,
-    decoration: TextDecoration.none);
 
 class MyApp extends StatefulWidget {
   @override
@@ -82,115 +70,66 @@ class _MyAppState extends State<MyApp> {
                   ),
                 ),
               ),
-              Column(
-                children: [
-                  SizedBox(
-                    height: 150,
-                    child: ListView(
-                      padding: EdgeInsets.fromLTRB(50, 0, 0,0),
-                      scrollDirection: Axis.horizontal,
-                      children: [
-                        NewWidget(s:'All',c:kinactiveColor),
-                        NewWidget(s:'Sofas',c:kactiveColor),
-                        NewWidget(s:'Bench',c:kinactiveColor),
-                        NewWidget(s:'Armchair',c:kinactiveColor),
-                      ],
-                    ),
-                  ),
-              Scrollbar(
-
+              SizedBox(
+                height: 60,
                 child: ListView(
-    children:[
-      Container(
-        child: Text('hi'),
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(0),
-          color: Color(0xFFefedf0),
-        ),
-      ),
-      Container(
-        child: Text('hi'),
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(0),
-          color: Color(0xFFefedf0),
-        ),
-      ),
-      Container(
-        child: Text('hi'),
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(0),
-          color: Color(0xFFefedf0),
-        ),
-      ),
-      Container(
-        child: Text('hi'),
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(0),
-          color: Color(0xFFefedf0),
-        ),
-      ),
-      Container(
-        child: Text('hi'),
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(0),
-          color: Color(0xFFefedf0),
-        ),
-      ),
-      Container(
-        child: Text('hi'),
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(0),
-          color: Color(0xFFefedf0),
-        ),
-      ),
-      Container(
-        child: Text('hi'),
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(0),
-          color: Color(0xFFefedf0),
-        ),
-      ),
-      Container(
-        child: Text('hi'),
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(0),
-          color: Color(0xFFefedf0),
-        ),
-      ),
-      Container(
-        child: Text('hi'),
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(0),
-          color: Color(0xFFefedf0),
-        ),
-      ),
-      Container(
-        child: Text('hi'),
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(0),
-          color: Color(0xFFefedf0),
-        ),
-      ),
-      Container(
-        child: Text('hi'),
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(0),
-          color: Color(0xFFefedf0),
-        ),
-      ),
-      Container(
-        child: Text('hi'),
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(0),
-          color: Color(0xFFefedf0),
-        ),
-      ),
-
-    ] ,
-    ),
+                  padding: EdgeInsets.fromLTRB(50, 0, 0, 0),
+                  scrollDirection: Axis.horizontal,
+                  children: [
+                    NewWidget(s: 'All', c: kinactiveColor),
+                    NewWidget(s: 'Sofas', c: kactiveColor),
+                    NewWidget(s: 'Bench', c: kinactiveColor),
+                    NewWidget(s: 'Armchair', c: kinactiveColor),
+                  ],
+                ),
               ),
-                ],
-              ),
+              Expanded(
+                child: Container(
+                  margin: EdgeInsets.only(top: 20),
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.only(
+                          topRight: Radius.circular(40),
+                          topLeft: Radius.circular(40)),
+                      color: Colors.white),
+                  child: ListView(
+                    children: [
+                      NewCard(
+                        sTitle: 'Chair',
+                        sLabel: Text(
+                          'This is chair',
+                          style: kcardLabelStyle,
+                        ),
+                        w: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQfFe-0ryf_a_CICuGMbF7LacjKXOvEFIeMVQ&usqp=CAU',
+                      ),
+                      NewCard(
+                        sTitle: 'Chair 2',
+                        sLabel: Text('Newgate ',style: kcardLabelStyle,),
+                        w: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTf9nGMTb5_v2UMXqVmDwRKFqKcMh9hnoGISg&usqp=CAU',
+                      ),
+                      NewCard(
+                        sTitle: 'Chair 2',
+                        sLabel: Text('Newgate ',style: kcardLabelStyle,),
+                        w: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTf9nGMTb5_v2UMXqVmDwRKFqKcMh9hnoGISg&usqp=CAU',
+                      ),
+                      NewCard(
+                        sTitle: 'Chair 2',
+                        sLabel: Text('Newgate ',style: kcardLabelStyle,),
+                        w: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTf9nGMTb5_v2UMXqVmDwRKFqKcMh9hnoGISg&usqp=CAU',
+                      ),
+                      NewCard(
+                        sTitle: 'Chair 2',
+                        sLabel: Text('Newgate ',style: kcardLabelStyle,),
+                        w: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTf9nGMTb5_v2UMXqVmDwRKFqKcMh9hnoGISg&usqp=CAU',
+                      ),
+                      NewCard(
+                        sTitle: 'Chair 2',
+                        sLabel: Text('Newgate ',style: kcardLabelStyle,),
+                        w: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTf9nGMTb5_v2UMXqVmDwRKFqKcMh9hnoGISg&usqp=CAU',
+                      ),
+                    ],
+                  ),
+                ),
+              )
             ],
           ),
         ),
@@ -199,10 +138,58 @@ class _MyAppState extends State<MyApp> {
   }
 }
 
+class NewCard extends StatelessWidget {
+  String sTitle;
+  Widget sLabel;
+  String w;
+  NewCard({this.sTitle, this.sLabel, this.w});
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      margin: EdgeInsets.all(20),
+      color: Colors.white,
+      elevation: 20.0,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(10),
+      ),
+      child: Padding(
+        padding: const EdgeInsets.all(15.0),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  sTitle,
+                  style: kcardStyle,
+                ),
+                sLabel,
+                SizedBox(
+                  height: 20,
+                ),
+                Container(
+                  color: Colors.red[100],
+                  child: Text('\$200'),
+                ),
+              ],
+            ),
+            Image(
+              image: NetworkImage(w, scale: 1.8),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
 class NewWidget extends StatelessWidget {
-   String s;
-   Color c;
-   NewWidget({this.s,this.c});
+  String s;
+  Color c;
+  NewWidget({this.s, this.c});
 
   @override
   Widget build(BuildContext context) {
@@ -210,12 +197,15 @@ class NewWidget extends StatelessWidget {
       decoration: BoxDecoration(
         color: c,
         borderRadius: BorderRadius.circular(10),
-        boxShadow:[BoxShadow(color: Colors.black12,blurRadius: 5)],
+        boxShadow: [BoxShadow(color: Colors.black12, blurRadius: 5)],
       ),
-      child: Text(s,style: ksmallStyle,),
+      child: Text(
+        s,
+        style: ksmallStyle,
+      ),
       height: 35,
       padding: EdgeInsets.all(10),
-      margin: EdgeInsets.fromLTRB(10, 10, 10, 100),
+      margin: EdgeInsets.fromLTRB(10, 10, 10, 10),
     );
   }
 }
